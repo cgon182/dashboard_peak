a0fc1c777c286ec47fd86bbfd35a1f692022261b
cgon182 <camilagon182@gmail.com>
2024-02-23 15:13:49 -0300

    dashboard finished without prices

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -3,25 +3,29 @@ import ContentRowCenter from './ContentRowCenter';
 import ContentRowMovies from './ContentRowMovies';
 import Chart from './Chart';
 
-function ContentRowTop(){
-    return(
-        <React.Fragment>
-				{/*<!-- Content Row Top -->*/}
-				<div className="container-fluid">
-					<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
-						<h1 className="h3 mb-0 text-gray-800">Panel de control</h1>
-					</div>
-				
-					{/*<!-- Content Row Movies-->*/}
-					<ContentRowMovies />
-					<ContentRowCenter />
-					<Chart />
-	
-				</div>
-				{/*<!--End Content Row Top-->*/}
+function ContentRowTop() {
+	return (
+		<React.Fragment>
+			{/*<!-- Content Row Top -->*/}
+			<div className="container-fluid">
+				<div className="d-sm-flex aligns-items-center justify-content-between mb-4">
+					<h1 className="h3 mb-0 text-gray-800">Panel de control</h1>
+					<a href="http://localhost:3000/inventory/create" class="admin-features">Añadir nuevo producto</a>
+					<a href="http://localhost:3000/inventory" class="admin-features">Editar o eliminar productos</a>
+					<a href="http://localhost:3000" class="admin-features">Volver a la web</a>
 
-        </React.Fragment>
-    )
+				</div>
+
+				{/*<!-- Content Row Movies-->*/}
+				<ContentRowMovies />
+				<ContentRowCenter />
+				<Chart />
+
+			</div>
+			{/*<!--End Content Row Top-->*/}
+
+		</React.Fragment>
+	)
 
 }
 export default ContentRowTop;
